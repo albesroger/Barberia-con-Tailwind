a878d678e352f18419f3f478427b652837818cd3
albesroger <albescaballero@gmail.com>
2025-03-14 09:11:17 -0400

    Pagina_Salon_Tailwin

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        fondo: "url('/IMGS/Salon2.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
