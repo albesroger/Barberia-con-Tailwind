4bfb553bb818b60e82ea702aab08b2275cf8828c
albesroger <albescaballero@gmail.com>
2025-03-28 16:25:03 -0400

    acualice servicios

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,2 +1,10 @@
-import usuario from "./usuario";
 
+// JavaScript para controlar el menú móvil
+document.addEventListener("DOMContentLoaded", function () {
+    const mobileMenuButton = document.getElementById("boton_menu_mobil");
+    const mobileMenu = document.getElementById("mobile-menu");
+
+    mobileMenuButton.addEventListener("click", function () {
+      mobileMenu.classList.toggle("hidden");
+    });
+});
